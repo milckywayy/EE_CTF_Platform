6ba0c75e1677f6189124d1bd91f711df3aa5516e
milckywayy <dmitriwear@gmail.com>
2024-07-13 00:30:37 +0200

    Made usosapi code thread-safe

--- usosapi/usosapi.py
+++ usosapi/usosapi.py
@@ -1,6 +1,7 @@
 import logging
 import time
 import rauth
+import threading
 
 _REQUEST_TOKEN_SUFFIX = 'services/oauth/request_token'
 _AUTHORIZE_SUFFIX = 'services/oauth/authorize'
@@ -37,57 +38,59 @@ class USOSAPISession:
             base_url=base_address
         )
 
-        # self.auth_sessions = {
-        #   request_token: {
-        #       secret: request_token_secret,
-        #       timestamp: time_since_created
-        #   }
-        # }
         self.auth_sessions = {}
+        self.auth_sessions_lock = threading.Lock()
 
         self.authorized_session = None
+        self.authorized_session_lock = threading.Lock()
 
     def get_auth_url(self, callback='oob'):
         params = {'oauth_callback': callback, 'scopes': self.scopes}
         token_tuple = self.service.get_request_token(params=params)
         request_token, request_token_secret = token_tuple
 
-        self.auth_sessions[request_token] = {'secret': request_token_secret, 'timestamp': time.time()}
+        with self.auth_sessions_lock:
+            self.auth_sessions[request_token] = {'secret': request_token_secret, 'timestamp': time.time()}
 
         return request_token, self.service.get_authorize_url(request_token)
 
     def authorize(self, request_token, pin):
         try:
-            request_token_secret = self.auth_sessions.pop(request_token)['secret']
+            with self.auth_sessions_lock:
+                request_token_secret = self.auth_sessions.pop(request_token)['secret']
         except KeyError:
             raise USOSAPIAuthorizationError('Invalid request token')
 
         try:
-            self.authorized_session = self.service.get_auth_session(
-                request_token,
-                request_token_secret,
-                params={'oauth_verifier': pin}
-            )
+            with self.authorized_session_lock:
+                self.authorized_session = self.service.get_auth_session(
+                    request_token,
+                    request_token_secret,
+                    params={'oauth_verifier': pin}
+                )
         except KeyError:
             raise USOSAPIAuthorizationError('Consumer key or token key does not match')
 
     def get_access_data(self):
-        return self.authorized_session.access_token, self.authorized_session.access_token_secret
+        with self.authorized_session_lock:
+            return self.authorized_session.access_token, self.authorized_session.access_token_secret
 
     def resume_session(self, access_token, access_token_secret):
-        self.authorized_session = self.service.get_session()
-        self.authorized_session.access_token = access_token
-        self.authorized_session.access_token_secret = access_token_secret
+        with self.authorized_session_lock:
+            self.authorized_session = self.service.get_session()
+            self.authorized_session.access_token = access_token
+            self.authorized_session.access_token_secret = access_token_secret
 
         if not self.is_session_authorized():
             raise USOSAPIAuthorizationError('Error resuming USOSAPI session')
 
     def fetch_from_service(self, service, **kwargs):
-        session = self.authorized_session
-        if session is None:
-            raise USOSAPISessionNotAuthorizedError('Trying to fetch data from not authorized USOSAPI session')
+        with self.authorized_session_lock:
+            session = self.authorized_session
+            if session is None:
+                raise USOSAPISessionNotAuthorizedError('Trying to fetch data from not authorized USOSAPI session')
 
-        response = session.post(service, params=kwargs, data={})
+            response = session.post(service, params=kwargs, data={})
 
         if not response.ok:
             response.raise_for_status()
@@ -114,22 +117,24 @@ class USOSAPISession:
             return False
 
     def close_session(self):
-        if self.authorized_session is None:
-            return
+        with self.authorized_session_lock:
+            if self.authorized_session is None:
+                return
 
-        self.fetch_from_service('services/oauth/revoke_token')
-        self.authorized_session = None
+            self.fetch_from_service('services/oauth/revoke_token')
+            self.authorized_session = None
 
     def cleanup_auth_sessions(self):
         expiration_time = 1800
         current_time = time.time()
 
-        expired_tokens = [token for token, details in self.auth_sessions.items() if
-                          current_time - details['timestamp'] > expiration_time]
+        with self.auth_sessions_lock:
+            expired_tokens = [token for token, details in self.auth_sessions.items() if
+                              current_time - details['timestamp'] > expiration_time]
 
-        expired_sessions_count = 0
-        for token in expired_tokens:
-            del self.auth_sessions[token]
-            expired_sessions_count += 1
+            expired_sessions_count = 0
+            for token in expired_tokens:
+                del self.auth_sessions[token]
+                expired_sessions_count += 1
 
         logging.info(f"Removed {expired_sessions_count} expired sessions")
