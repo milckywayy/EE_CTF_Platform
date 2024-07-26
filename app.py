726fc8ee3cc114cfb9d62c705b064bb6150d1ec8
milckywayy <dmitriwear@gmail.com>
2024-07-26 14:40:37 +0200

    Removed unused rate limiter code

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from functools import wraps
 import requests
 from flask import Flask, render_template, redirect, url_for, request, session, flash, jsonify, send_from_directory
 from flask_babel import Babel, gettext as _
-# from flask_limiter import Limiter
 
 from usosapi.usosapi import USOSAPISession, USOSAPIAuthorizationError
 from model import db, User, Challenge, Solve, Rating, Comment
@@ -23,20 +22,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['BABEL_DEFAULT_LOCALE'] = 'pl'
 app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'pl']
-
-
-# def get_ip_address():
-#     if request.headers.get('X-Forwarded-For'):
-#         return request.headers.get('X-Forwarded-For').split(',')[0]
-#     return request.remote_addr
-#
-#
-# limiter = Limiter(
-#     get_ip_address,
-#     app=app,
-#     default_limits=["1000 per day", "500 per hour", "30 per minute"],
-#     headers_enabled=True
-# )
 
 babel = Babel(app)
 db.init_app(app)
